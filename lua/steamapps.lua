088c35a9734a93a1452f7c1f2aa3e0a0b0ba9e61
wellbutteredtoast <swatsonjones@mun.ca>
2026-01-05 21:30:24 -0330

    steamapps partial impl, just is_app_installed

--- lua/steamapps.lua
+++ lua/steamapps.lua
@@ -13,4 +13,22 @@
 
 local steamapps = {}
 
+local ffi = require("ffi")
+
+ffi.cdef[[
+    bool sl_get_dlc_data_by_index(int i_dlc, unsigned int* app_id, bool* available);
+    bool sl_is_app_installed(unsigned int app_id);
+]]
+
+function steamapps.get_dlc_data_by_idx(dlc_index)
+    -- not yet implemented, error!
+    return nil, "getDLCDataByIndex is not yet implemented."
+end
+
+function steamapps.is_app_installed(app_id)
+    local installed = ffi.new("bool[1]")
+    local result = ffi.C.sl_is_app_installed(app_id)
+    return result
+end
+
 return steamapps
