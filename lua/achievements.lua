1ef8cab7bc51db0a783f61744f749391c5f8f983
wellbutteredtoast <swatsonjones@mun.ca>
2026-01-06 19:19:20 -0330

    Implemented clear/get achievement in lua

--- lua/achievements.lua
+++ lua/achievements.lua
@@ -17,6 +17,9 @@ local ffi = require("ffi")
 ffi.cdef[[
     bool sl_RequestUserStats();
     bool sl_ResetAllStats(bool achievements_too);
+    bool sl_ClearAchievement(const char *achievement_id);
+    bool sl_GetAchievement(const char *achievement_id, bool *achieved);
+    bool sl_GetAchievementAchievedPercent(const char *achievement_id, float *percent);
 ]]
 
 local lib = ffi.load("steamlua")
@@ -32,6 +35,31 @@ function achievement.ResetAllStats(achievements_too)
     end
 
     return lib.sl_ResetAllStats(achievements_too)
+end
+
+function achievement.ClearAchievement(achievement_id)
+    if type(achievement_id) ~= "string" then
+        error("AchievementID needs to be a string", 2)
+    end
+
+    local achid = ffi.new("bool[1]")
+    local result = lib.sl_ClearAchievement(achid)
+    return result
+end
+
+function achievement.GetAchievement(achievement_id, achieved)
+    if type(achievement_id) ~= "string" then
+        error("AchievementID needs to be a string", 2)
+    end
+
+    if type(achieved) ~= "boolean" then
+        error("'achieved' variable must be a boolean", 2)
+    end
+
+    local achid_p = ffi.new("char[1]")
+    local achieve_p = ffi.new("bool[1]")
+
+    return lib.sl_GetAchievement(achid_p, achieve_p)
 end
 
 return achievement
