b26f6b860ff88c40b3a19022756731eed8991082
wellbutteredtoast <swatsonjones@mun.ca>
2026-01-05 21:32:27 -0330

    initial funcs for steam stats implmented

--- lua/achievements.lua
+++ lua/achievements.lua
@@ -12,4 +12,24 @@
 
 local achievement = {}
 
+local ffi = require("ffi")
+
+ffi.cdef[[
+    bool sl_request_current_stats();
+    bool sl_reset_all_stats(bool achievements_too);
+]]
+
+function achievement.request_current_stats()
+    return ffi.C.sl_request_current_stats()
+end
+
+function achievement.reset_all_stats(achievements_too)
+    if type(achievements_too) ~= "boolean" then
+        print("Expected boolean, got " .. type(achievements_too) .. "(reset_all_stats)")
+        return nil
+    end
+
+    return ffi.C.sl_reset_all_stats(achievements_too)
+end
+
 return achievement
