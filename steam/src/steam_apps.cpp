7d1c2445f4dbcfb084f17c297f419c9b2d844a0a
wellbutteredtoast <swatsonjones@mun.ca>
2026-01-05 22:37:58 -0330

    Removed DLC function as its not necessary at this time

--- steam/src/steam_apps.cpp
+++ steam/src/steam_apps.cpp
@@ -4,13 +4,14 @@
 
 extern "C" {
 
-EXPORT bool sl_GetDLCDataByIndex(int i_dlc, unsigned int app_id, bool available, char name, int name_buf_size) {
-    return SteamApps()->BGetDLCDataByIndex(i_dlc, &app_id, &available, &name, name_buf_size);
-}
-
 // Checks if app_id is installed locally, this does not check if the user OWNS the app.
 EXPORT bool sl_IsAppInstalled(unsigned int app_id) {
     return SteamApps()->BIsAppInstalled(app_id);
+}
+
+// Checks if the current user is subscribed to the game (has a license to play)
+EXPORT bool sl_IsSubscribed() {
+    return SteamApps()->BIsSubscribed();
 }
 
 } // extern "C"
