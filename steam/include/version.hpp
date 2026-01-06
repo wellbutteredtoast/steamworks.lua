4ea5a664e2867bc1667f207919679abbd36cbaa0
wellbutteredtoast <swatsonjones@mun.ca>
2026-01-05 22:46:35 -0330

    ver bump, release imminent!

--- steam/include/version.hpp
+++ steam/include/version.hpp
@@ -2,7 +2,7 @@
 #define VERSION_HPP
 
 // steamworks.lua and its luasteam library follow Semantic Versioning.
-#define LUASTEAM_VERSION "0.1.0"
+#define LUASTEAM_VERSION "1.0.0"
 #define LUASTEAM_ABI_VERSION 1
 
 #endif // VERSION_HPP
