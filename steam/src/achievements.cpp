3df37c85b41daa09d5c3f43e4f7ebc8cffa41882
wellbutteredtoast <swatsonjones@mun.ca>
2026-01-06 18:12:06 -0330

    A couple achievement based funcs have been wrapped

--- steam/src/achievements.cpp
+++ steam/src/achievements.cpp
@@ -2,9 +2,6 @@
 #include "steam/steam_api.h"
 #include <iostream>
 
-// Note: this also handles some aspects of SteamUserStats() found in ISteamUserStats
-//       refer to Valve's documentation for more details on this class
-
 extern "C" {
 
 // The user_id should not change during runtime. That's... really bad.
@@ -17,6 +14,23 @@ EXPORT bool sl_RequestUserStats() {
 // Note: achievements_too defaults to false to prevent accidental resets of achievements.
 EXPORT bool sl_ResetAllStats(bool achievements_too=false)  {
     return SteamUserStats()->ResetAllStats(achievements_too);
+}
+
+EXPORT bool sl_ClearAchievement(const char *achievement_id) {
+    return SteamUserStats()->ClearAchievement(achievement_id);
+}
+
+// -> SteamAPICall_t DownloadLeaderboardEntries( SteamLeaderboard_t hSteamLeaderboard, ELeaderboardDataRequest eLeaderboardDataRequest, int nRangeStart, int nRangeEnd );
+// -> SteamAPICall_t DownloadLeaderboardEntriesForUsers( SteamLeaderboard_t hSteamLeaderboard, CSteamID *prgUsers, int cUsers );
+// -> SteamAPICall_t FindLeaderboard( const char *pchLeaderboardName );
+// -> SteamAPICall_t FindOrCreateLeaderboard( const char *pchLeaderboardName, ELeaderboardSortMethod eLeaderboardSortMethod, ELeaderboardDisplayType eLeaderboardDisplayType );
+
+EXPORT bool sl_GetAchievement(const char *achievement_id, bool *achieved) {
+    return SteamUserStats()->GetAchievement(achievement_id, achieved);
+}
+
+EXPORT bool sl_GetAchievementAchievedPercent(const char *achievement_id, float *percent) {
+    return SteamUserStats()->GetAchievementAchievedPercent(achievement_id, percent);
 }
 
 } // extern "C"
